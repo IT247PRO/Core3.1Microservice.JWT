53461730a6c63b67057a3817de17bd00d2f8758d
Muhammad Azhar <azhar.cs@outlook.com>
2020-06-03 17:22:10 -0500

    .net core microservices JWT example
    .net core microservices JWT example

---
+++ src/AuthService/Controllers/AuthorizeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AuthService.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizeController : ControllerBase
+    {
+       
+        // POST: api/Authorize
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post([FromBody] User user)
+        {
+            return Ok(GenerateJWTToken(user));
+        }
+
+        private User GenerateJWTToken(User user)
+        {
+            /**
+             * VALIDATE USER from Datasource, before creating the token.
+             */
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("somethingyouwantwhichissecurewillworkk"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "test user"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+            var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: credentials
+            );
+            user.token = new JwtSecurityTokenHandler().WriteToken(token);
+            return user;
+        }
+
+
+    }
+}
